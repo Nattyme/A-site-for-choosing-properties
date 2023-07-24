e563bd87b9342e6294dc60af084a7a71305b7f22
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-07-24 21:15:58 +0300

    HTML/CSS/JS
    - btn-more shows three more cards on click

--- js/main.js
+++ js/main.js
@@ -2,9 +2,19 @@ const sidebarToggleBtn = document.querySelector('.menu-icon-wrapper');
 const menuIcon = document.querySelector('.menu-icon');
 const sidebar = document.querySelector('.sidebar');
 
+
 sidebarToggleBtn.onclick = function () {
     menuIcon.classList.toggle('menu-icon-active');
     sidebar.classList.toggle('sidebar--mobile-active');
 };
 
 // .sidebar--mobile-active
+const btnShowMoreCards = document.querySelector('.btn-more');
+const hiddenCards = document.querySelectorAll('.cards-link--hidden');
+
+btnShowMoreCards.addEventListener('click', function () {
+    hiddenCards.forEach(function(cards) {
+        cards.classList.remove('cards-link--hidden');
+    })
+
+} );
