200f5f57484851f3cb26745153961a781098156e
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-07-25 18:32:02 +0300

    JS
    added a polyfill for IE

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+/**
+ * NodeList.prototype.forEach() polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
+ */
+if (window.NodeList && !NodeList.prototype.forEach) {
+	NodeList.prototype.forEach = function (callback, thisArg) {
+		thisArg = thisArg || window;
+		for (var i = 0; i < this.length; i++) {
+			callback.call(thisArg, this[i], i, this);
+		}
+	};
+}
+
 /* filter for mobile devices*/
 const sidebarToggleBtn = document.querySelector('.menu-icon-wrapper');
 const menuIcon = document.querySelector('.menu-icon');
